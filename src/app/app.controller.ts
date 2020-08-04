cbfa9d83f336d8a0ccbf1d80ecd73e9a7e25b92a
blocksan <sandyghosh555@gmail.com>
2020-08-04 13:24:16 +0400

    feat: microservice setup completegit

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,23 +1,32 @@
-import { Controller, Get, Inject, UseGuards } from '@nestjs/common';
-import { AppService } from './app.service';
-import { ConfigService } from '@nestjs/config';
+import { Controller} from '@nestjs/common';
 import { ApplicationLoggerService } from 'src/logger/logger.service';
-import { JwtAuthGuard } from 'src/auth/jwt/jwt-auth.guard';
+import { Client, Transport, ClientProxy, MessagePattern, EventPattern } from '@nestjs/microservices';
 
 @Controller('app')
 export class AppController {
-  constructor(private readonly appService: AppService, private appLogger: ApplicationLoggerService) {
+  @Client({ transport: Transport.NATS })
+  client: ClientProxy;
+
+  constructor(private appLogger: ApplicationLoggerService) {
    this.appLogger.setContext('AppController')
   }
 
-  @Inject()
-  private configService: ConfigService
 
-  @UseGuards(JwtAuthGuard)
-  @Get()
-  getHello(): string {
-    this.appLogger.log(`AppController in controller ${this.configService.get("DB_TYPE")}`)
-    return this.appService.getHello();
+  async onApplicationBootstrap() {
+    await this.client.connect();
+  }
+
+  @MessagePattern('via_http_message')
+  async sendMessage(message: string) {
+    this.appLogger.log(`message received from the client ${message}`)
+    this.client.emit<number>('micro_2_micro_message','Hello from microservice')
+    return message
+  }
+
+  @EventPattern('micro_2_micro_message')
+  showMessage(message: string) {
+    this.appLogger.log(`emitting event to the client ${message}`)
+    this.client.emit<number>('via_micro_event',message)
   }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { Logger, ValidationPipe } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { ApplicationLoggerService } from './logger/logger.service';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('/api')
-
-  app.useLogger(new ApplicationLoggerService())
-  app.useGlobalPipes(new ValidationPipe({disableErrorMessages:false,dismissDefaultMessages:false}));
-
-  const options = new DocumentBuilder()
-    .setTitle('Backend Service')
-    .setDescription('The backend service API description')
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, options);
-
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.NATS,
+      options: {
+        url: process.env.NATS_SERVER,
+      },
+    },
+  );
   /**
    * import port from the configuration
    */
 
-  const configService = app.get(ConfigService)
-  const port = configService.get('APP_PORT')
-  SwaggerModule.setup('swagger', app, document);
-  await app.listen(port,() => {
-    Logger.log(`Service started at ${port} port`)
-  });
+  app.useLogger(new ApplicationLoggerService())
+  app.useGlobalPipes(new ValidationPipe({ disableErrorMessages: false, dismissDefaultMessages: false }));
+
+  app.listen(() => Logger.log(`Microservice started`));
 }
 bootstrap();
